e1be6151dec3aec3b6fbb343331c52688ccf1806
ohsky97 <ohsky97@gmail.com>
2020-09-29 21:54:43 +0900

    Updates

---
+++ precache-manifest.75547a06dfbc2f9bbef9543c9d6c6e43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fad322807f388879ae20d99a0c00a41",
+    "url": "/hs-shopping-cart/index.html"
+  },
+  {
+    "revision": "1a163907a2baeb1cbcef",
+    "url": "/hs-shopping-cart/static/css/2.51108769.chunk.css"
+  },
+  {
+    "revision": "bb4ab390f2fe0d40ddc1",
+    "url": "/hs-shopping-cart/static/css/main.fee2d048.chunk.css"
+  },
+  {
+    "revision": "1a163907a2baeb1cbcef",
+    "url": "/hs-shopping-cart/static/js/2.9035e8cd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hs-shopping-cart/static/js/2.9035e8cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb4ab390f2fe0d40ddc1",
+    "url": "/hs-shopping-cart/static/js/main.7d06d285.chunk.js"
+  },
+  {
+    "revision": "55bedc33f069f88f144a",
+    "url": "/hs-shopping-cart/static/js/runtime-main.cdb8378e.js"
+  }
+]);
