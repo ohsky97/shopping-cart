2f13c0f59dd91405b3fe7a8c4d02136e128bf08f
ohsky97 <ohsky97@gmail.com>
2020-10-05 19:17:44 +0900

    Context explain

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -3,6 +3,11 @@ import { CartReducer, sumItems } from './CartReducer';
 
 
 // 장바구니 이벤트 타입 설정(Store, Cart에서 쓰임)
+// Context 란?
+// React에서 데이터를 부모에서 자식에게 props를 통해 전달되지만,
+// 여러 컴포넌트들에게 전달해야하는 props의 경우 과정이 번거로워진다.
+// 이 때, context를 사용하면 트리 단계마다 명시적으로 props를 넘겨주지 않아도
+// 다른 컴포넌트들이 데이터를 공유할 수 있다.
 export const CartContext = createContext();
 
 const storage = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
